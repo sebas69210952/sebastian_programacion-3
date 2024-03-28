7954124cca6d38ddb40397871d75c97f5e71a533
sebas69210952 <132626189+sebas69210952@users.noreply.github.com>
2024-03-27 21:37:15 -0400

    Add files via upload

---
+++ clase numeros/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Numeros.ejercicio1();
+        Numeros.ejercicio2();
+        Numeros.ejercicio3();
+        Numeros.ejercicio4();
+        Numeros.ejercicio5();
+    }
+}
